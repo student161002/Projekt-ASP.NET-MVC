54aa726ee9f79f8734a2c8e3ba45432376058d36
student161002 <elezargaming@gmail.com>
2026-01-05 17:06:28 +0100

    Utworzony projekt w VisualStudio i zsynchronizowany z repozytorium git

---
+++ Sklep_Internetowy/Sklep_Internetowy/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Sklep_Internetowy.Models;
+
+namespace Sklep_Internetowy.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
