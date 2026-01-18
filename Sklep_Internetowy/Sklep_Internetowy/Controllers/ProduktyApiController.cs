d33301842602c725134223eda2f541f9959e32a6
student161002 <elezargaming@gmail.com>
2026-01-18 19:33:13 +0100

    Zmaiana wyglądu strony przez bootstrap.css Implementacja procesu składania zamówień miejsze poprawki

---
+++ Sklep_Internetowy/Sklep_Internetowy/Controllers/ProduktyApiController.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Sklep_Internetowy.Models;
+
+namespace Sklep_Internetowy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProduktyApiController : ControllerBase
+    {
+        private readonly SklepDbContext _context;
+
+        public ProduktyApiController(SklepDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/ProduktyApi
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Produkty>>> GetProdukties()
+        {
+            return await _context.Produkties.ToListAsync();
+        }
+
+        // GET: api/ProduktyApi/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Produkty>> GetProdukty(int id)
+        {
+            var produkty = await _context.Produkties.FindAsync(id);
+
+            if (produkty == null)
+            {
+                return NotFound();
+            }
+
+            return produkty;
+        }
+
+        // PUT: api/ProduktyApi/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutProdukty(int id, Produkty produkty)
+        {
+            if (id != produkty.ProduktId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(produkty).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProduktyExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/ProduktyApi
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Produkty>> PostProdukty(Produkty produkty)
+        {
+            _context.Produkties.Add(produkty);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetProdukty", new { id = produkty.ProduktId }, produkty);
+        }
+
+        // DELETE: api/ProduktyApi/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProdukty(int id)
+        {
+            var produkty = await _context.Produkties.FindAsync(id);
+            if (produkty == null)
+            {
+                return NotFound();
+            }
+
+            _context.Produkties.Remove(produkty);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ProduktyExists(int id)
+        {
+            return _context.Produkties.Any(e => e.ProduktId == id);
+        }
+    }
+}
---
+++ Sklep_Internetowy/Sklep_Internetowy/Controllers/ZamowieniaController.cs
@@ -0,0 +1,206 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Sklep_Internetowy.Models;
+
+namespace Sklep_Internetowy.Controllers
+{
+    public class ZamowieniaController : Controller
+    {
+        private readonly SklepDbContext _context;
+
+        public ZamowieniaController(SklepDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Zamowienia
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Zamowienia.ToListAsync());
+        }
+
+        // GET: Zamowienia/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zamowienium = await _context.Zamowienia
+                .FirstOrDefaultAsync(m => m.ZamowienieId == id);
+            if (zamowienium == null)
+            {
+                return NotFound();
+            }
+
+            return View(zamowienium);
+        }
+
+		// GET: Zamowienia/Create
+		// GET: Zamowienia/Create?produktId=5
+		public IActionResult Create(int? produktId)
+		{
+			if (produktId == null)
+			{
+				return NotFound();
+			}
+
+			var produkt = _context.Produkties.Find(produktId);
+
+			if (produkt == null)
+			{
+				return NotFound();
+			}
+
+			ViewBag.ProduktNazwa = produkt.Nazwa;
+			ViewBag.ProduktCena = produkt.Cena;
+			ViewBag.ProduktId = produkt.ProduktId;
+
+			return View();
+		}
+		// GET: Zamowienia/Potwierdzenie/5
+		public IActionResult Potwierdzenie(int id)
+		{
+			return View(id); // Przekazujemy ID zamówienia do widoku
+		}
+
+		// POST: Zamowienia/Create
+		// To protect from overposting attacks, enable the specific properties you want to bind to.
+		// For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+		// POST: Zamowienia/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create(int ProduktId, [Bind("ZamowienieId,DataZamowienia,AdresWysylki,ImieNazwiskoKlienta")] Zamowienium zamowienie)
+		{
+			zamowienie.DataZamowienia = DateTime.Now;
+
+			ModelState.Remove("PozycjeZamowienia");
+
+			if (ModelState.IsValid)
+			{
+				_context.Add(zamowienie);
+				await _context.SaveChangesAsync(); 
+
+				var produkt = await _context.Produkties.FindAsync(ProduktId);
+
+				if (produkt != null)
+				{
+					
+					var pozycja = new PozycjeZamowienium
+					{
+						ZamowienieId = zamowienie.ZamowienieId,
+						ProduktId = ProduktId,
+						Ilosc = 1, 
+						CenaJednostkowa = produkt.Cena
+					};
+
+					_context.Add(pozycja);
+
+					zamowienie.WartoscCalkowita = produkt.Cena;
+					_context.Update(zamowienie);
+
+					await _context.SaveChangesAsync();
+				}
+
+				return RedirectToAction("Potwierdzenie", new { id = zamowienie.ZamowienieId });
+			}
+
+			return View(zamowienie);
+		}
+
+		// GET: Zamowienia/Edit/5
+		public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zamowienium = await _context.Zamowienia.FindAsync(id);
+            if (zamowienium == null)
+            {
+                return NotFound();
+            }
+            return View(zamowienium);
+        }
+
+        // POST: Zamowienia/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ZamowienieId,DataZamowienia,AdresWysylki,ImieNazwiskoKlienta,WartoscCalkowita")] Zamowienium zamowienium)
+        {
+            if (id != zamowienium.ZamowienieId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(zamowienium);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ZamowieniumExists(zamowienium.ZamowienieId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(zamowienium);
+        }
+
+        // GET: Zamowienia/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var zamowienium = await _context.Zamowienia
+                .FirstOrDefaultAsync(m => m.ZamowienieId == id);
+            if (zamowienium == null)
+            {
+                return NotFound();
+            }
+
+            return View(zamowienium);
+        }
+
+        // POST: Zamowienia/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var zamowienium = await _context.Zamowienia.FindAsync(id);
+            if (zamowienium != null)
+            {
+                _context.Zamowienia.Remove(zamowienium);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ZamowieniumExists(int id)
+        {
+            return _context.Zamowienia.Any(e => e.ZamowienieId == id);
+        }
+    }
+}
